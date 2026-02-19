bf5681cf2e0104e03b9a9c9f29ea0ab38ba60752
ayahusseini <aya.ahusseini@gmail.com>
2026-02-19 22:37:34 +0000

    added validation

--- src/probability_simulator/coin_flips.py
+++ src/probability_simulator/coin_flips.py
@@ -1,32 +1,51 @@
 """Module for simulating coin flips"""
-import numpy as np 
+
+import numpy as np
+
 
 class Coin:
     """Class representing a coin"""
 
-    def __init__(self, bias=0.5, rng= None):
+    def __init__(self, bias=0.5, rng=None, initial_state=None):
         """Initialize the coin with a given bias
-        
+
         bias: probability of landing on heads (1), default is 0.5 for a fair coin
         """
         self.bias = bias
-        self.state = 1
         self.rng = np.random.default_rng() if rng is None else rng
+        self.state = initial_state if initial_state else self.flip()
+
+    @staticmethod
+    def _validate_params(bias, initial_state):
+        """Raise an error if a parameter is invalid"""
+        if not (isinstance(bias, float) or (bias is None)):
+            raise TypeError(
+                f"Bias should be a float or None. Supplied {type(bias)}, {bias}"
+            )
+        if isinstance(bias, float) and (not 0 <= bias <= 1):
+            raise ValueError(f"Bias should be in the range [0,1]. Supplied {bias}")
+        if not (isinstance(initial_state, int) or (initial_state is None)):
+            raise ValueError(
+                f"initial_state should be an integer (0 or 1) or None. Supplied {type(initial_state)}, {initial_state}"
+            )
+        if isinstance(initial_state, int) and (not int in [0, 1]):
+            raise ValueError(
+                f"initial_state should be an integer (0 or 1). Supplied {initial_state}"
+            )
 
     def flip(self):
         """Simulate flipping the coin"""
         self.state = 1 if self.rng.random() < self.bias else 0
         return self.state
-    
+
     def flip_n(self, n: int):
         """Simulate flipping the coin n times"""
         return (self.rng.random(n) < self.bias).astype(int)
 
     def __str__(self) -> str:
         """String representation of the coin"""
-        return f"Coin(bias={self.bias})"
+        return "H" if self.state == 1 else "T"
 
     def __repr__(self) -> str:
         """Official string representation of the coin"""
-        return "H" if self.state == 1 else "T"
-        
+        return f"Coin(bias={self.bias})"
--- src/probability_simulator/tests/test_coin_flips.py
+++ src/probability_simulator/tests/test_coin_flips.py
@@ -2,38 +2,53 @@ import numpy as np
 from pytest import fixture
 from probability_simulator.coin_flips import Coin
 
+
 @fixture
 def fair_coin():
     """Fixture for creating a coin instance"""
-    return Coin(bias = 0.5)
+    return Coin(bias=0.5)
+
+
+def test_text_representation(fair_coin):
+    assert f"{fair_coin}" in ["H", "T"]
+
+
+def test_coin_with_supplied_initial_state():
+    c = Coin(initial_state=1)
+    assert c.state == 1
+
 
 def test_coin_flip(fair_coin):
     """Test that a fair coin flip returns either 0 or 1"""
     assert fair_coin.flip() in [0, 1]
-    
+
+
 def test_coin_flip_n_is_array(fair_coin):
     """Test that many fair coin flips return an array"""
-    assert isinstance(fair_coin.flip_n(n = 12),np.ndarray) 
-    
+    assert isinstance(fair_coin.flip_n(n=12), np.ndarray)
+
+
 def test_coin_flip_n_is_array_of_1s_and_0s(fair_coin):
     """Test that many fair coin flips return an array of 1s and 0s"""
-    result = fair_coin.flip_n(n = 12)
+    result = fair_coin.flip_n(n=12)
     assert all(x in [0, 1] for x in result)
+
 
 def test_two_different_rngs_give_different_results():
     """Test that two different RNGs give different results"""
-    coin1 = Coin(bias = 0.5)
-    coin2 = Coin(bias = 0.5)
-    result1 = coin1.flip_n(n = 100)
-    result2 = coin2.flip_n(n = 100)
+    coin1 = Coin(bias=0.5)
+    coin2 = Coin(bias=0.5)
+    result1 = coin1.flip_n(n=100)
+    result2 = coin2.flip_n(n=100)
     assert not np.array_equal(result1, result2)
+
 
 def test_same_rngs_give_same_results():
     """Test that two different RNGs give different results"""
     r1 = np.random.default_rng(seed=42)
     r2 = np.random.default_rng(seed=42)
-    coin1 = Coin(bias = 0.5, rng = r1)
-    coin2 = Coin(bias = 0.5, rng = r2)
-    result1 = coin1.flip_n(n = 100)
-    result2 = coin2.flip_n(n = 100)
+    coin1 = Coin(bias=0.5, rng=r1)
+    coin2 = Coin(bias=0.5, rng=r2)
+    result1 = coin1.flip_n(n=1000)
+    result2 = coin2.flip_n(n=1000)
     assert np.array_equal(result1, result2)
