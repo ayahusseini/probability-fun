8974d91e19ba8a70a9db74757a54c1b4363613ab
ayahusseini <aya.ahusseini@gmail.com>
2026-02-21 11:52:35 +0000

    added testing

--- src/probability_simulator/tests/test_validate_fn.py
+++ src/probability_simulator/tests/test_validate_fn.py
@@ -10,3 +10,20 @@ from probability_simulator.utility import is_callable, in_inclusive_range
 )
 def test_is_callable(inputfn, exp):
     assert is_callable(inputfn) is exp
+
+
+@pytest.mark.parametrize(
+    "n, minc, maxc, exp", [
+        (1,1,1,True),
+        (1,2,3, False),
+        (1,2,-1, False),
+        (1, -1, 1, True),
+        (1, 0, 10, True),
+        (1, None, 10, True),
+        (1, None, -1 , False),
+        (1, 0, None, True), 
+        (1, 2, None, False)
+    ]
+)
+def test_in_inclusive_range(n, minc, maxc, exp):
+    assert in_inclusive_range(n,minc,maxc) == exp
